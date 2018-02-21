72173863f727e4a3e7fb611162af0b46bca56f20
Skellora <pascal_noel@hotmail.com>
2018-02-21 18:55:51 +0000

    Startup the web page serve-y on another thread in main

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ extern crate metrolti_lib;
 
 use metrolti_lib::server as server;
 use metrolti_lib::metro_game as game;
+use metrolti_lib::web as web;
+
+use std::thread;
 
 pub fn main() {
+    thread::spawn(|| web::startup_web_frontend("localhost:3005".to_string(), "localhost:3004".to_string()));
     server::listen::<game::MetroGame>("localhost:3004".to_string(), String::new());
 }
 
