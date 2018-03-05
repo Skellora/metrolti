d6636dc0bd352eaa0c4e058ad9f7f6c5618e3582
Skellora <pascal_noel@hotmail.com>
2018-03-05 20:21:31 +0000

    Need to centre the square for the border

--- www/static/metro.js
+++ www/static/metro.js
@@ -37,12 +37,12 @@ let glShapes = (function() {
 
   let square = function(gl) {
     const vertices = [
-      0, 0,
-      0, 1,
-      1, 0,
-      1, 1,
-      1, 0,
-      0, 1
+      -0.5, -0.5,
+      -0.5, 0.5,
+      0.5, -0.5,
+      0.5, 0.5,
+      0.5, -0.5,
+      -0.5,  0.5
     ];
     return {
       vertices: bufferFromVertices(gl, vertices),
