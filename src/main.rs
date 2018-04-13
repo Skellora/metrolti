50cb90017ac8d32ed6b4608053c65e290b3256c2
Skellora <pascal_noel@hotmail.com>
2018-04-13 08:16:02 +0100

    As the edges are, demo player doing them the other way is nicer

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,8 @@ fn demo_player(addr: &str) {
         DemoAction::WaitTime(2),
         DemoAction::Act(game::PlayerAction::StartGame),
         DemoAction::WaitTime(2),
-        DemoAction::Act(game::PlayerAction::ConnectStations(game::StationId(1), game::StationId(0))),
-        DemoAction::Act(game::PlayerAction::ConnectStations(game::StationId(0), game::StationId(2))),
+        DemoAction::Act(game::PlayerAction::ConnectStations(game::StationId(2), game::StationId(0))),
+        DemoAction::Act(game::PlayerAction::ConnectStations(game::StationId(0), game::StationId(1))),
     ];
     let mut actions = actions_list.iter();
     let mut curr = actions.next();
